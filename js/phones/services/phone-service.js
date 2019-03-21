2b26059600befca225b865b3ab88c781346118f6
OlegBabiuk <42335942+OlegBabiuk@users.noreply.github.com>
2019-03-21 11:29:26 +0200

    Update phone-service.js

--- js/phones/services/phone-service.js
+++ js/phones/services/phone-service.js
@@ -1,16 +1,16 @@
 
-const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
+const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static';
 
 const PhoneService = {
 
 
   getAll() {
-    return fetch(`${BASE_URL}/phones/phones.json`)
+    return fetch(`${BASE_URL}/api/phones.json`)
       .then(response => response.json());
   },
 
   getById(id) {
-    return fetch(`${BASE_URL}/phones/${id}.json`)
+    return fetch(`${BASE_URL}/api/${id}.json`)
       .then(response => response.json());
   },
 };
